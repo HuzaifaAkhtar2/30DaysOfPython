bf65e4f75bb3aa530e9b5e737bb42c151b56fbc4
HuzaifaAkhtar2 <huzaifaakh@outlook.com>
2025-11-05 21:32:15 +0500

    Day 15 Complete

---
+++ day_15/main.py
@@ -0,0 +1,34 @@
+# Open you python interactive shell and try all the examples covered in this section.
+
+SyntaxError
+# print "hi"
+
+NameError
+# print(age)
+
+IndexError
+lst = [0,1,2,3,4,5]
+print(lst[6])
+
+ModuleNotFoundError
+# import functiontools
+
+AttributeError
+from math import pi
+# print(math.PI)
+
+KeyError
+dictionary = {"name": "huzaifa"}
+print(dictionary["nam"])
+
+TypeError
+print(3 + "3")
+
+ImportError
+from math import power
+
+ValueError
+print(int("abc"))
+
+ZeroDivisionError
+print(1/0)
